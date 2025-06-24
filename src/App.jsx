f210c9eeeb6dffe663e6fadc8f8b9d2fb42dd2a0
GabrielLuis122002 <gabrielluisluna12@gmail.com>
2025-06-25 03:03:07 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+
+  return (
+    <>
+    <div className="flex justify-center items-center h-screen bg-bg">
+  <h1 className="text-7xl font-bold  text-primary">Welcome to Gaming Hub</h1>
+    </div>
+   
+    </>
+  )
+}
+
+export default App
